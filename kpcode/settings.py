26be6d2772d5a1327ff7a2f2914c1d4dd0c074f6
Kuldeep Saini <mailtokuldeepsaini@gmail.com>
2021-01-31 23:36:09 +0530

    debug false

--- kpcode/settings.py
+++ kpcode/settings.py
@@ -13,23 +13,18 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mj0*p&*javn0i8nkl*)by2*bic0)q9_2vngh(wj0oy&e&6&hr#'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get('DEBUG_VALUE',False)
 
 
-# Application definition
+ALLOWED_HOSTS = ['https://kpcode.herokuapp.com/','127.0.0.1']
+
+
 
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
@@ -73,8 +68,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kpcode.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
@@ -84,8 +78,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -103,8 +95,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -117,8 +107,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
